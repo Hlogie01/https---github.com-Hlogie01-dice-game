289193325c31842bd168e2fecf44c2ab33b50665
Kimberly Tapfumaneyi <kimberlytapfumanyi16@gmail.com>
2024-05-01 15:54:51 +0200

    First commit

---
+++ main.js
@@ -0,0 +1,18 @@
+// initial player names and leaderboard object
+let player1 = "Player1";
+let player2 = "Player2";
+let leaderboard = {};
+
+// Edit names function
+function editNames() {
+    // Prompt users to enter player names
+    player1 = prompt("Please enter player 1's name");
+    player2 = prompt("Please enter player 2's name");
+
+    // Check if both names are valid
+    if (!player1 || !player2) {
+        alert("Please enter valid names for both players.");
+        return;
+    }
+
+    // Update player names dis
