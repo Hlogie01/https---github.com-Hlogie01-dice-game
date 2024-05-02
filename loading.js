3bac9cb1547c5a04a951afc63b4f6fb66b87f579
MphoVersace <mtlomelompho@gmail.com>
2024-05-02 21:07:08 +0200

    loading update

---
+++ loading.js
@@ -0,0 +1,22 @@
+// Use setTimeout to execute a function after a specified delay
+// this is a built-in function in JavaScript
+let countdown = 5; // Change the countdown value to 5
+
+// Display the initial countdown
+updateCountdown();
+
+const countdownInterval = setInterval(function () {
+  countdown -= 1;
+  updateCountdown();
+
+  if (countdown <= 0) {
+    clearInterval(countdownInterval);
+    window.location.href = "player.html";
+  }
+}, 1000); // Update the countdown every 1000 milliseconds (1 second)
+
+function updateCountdown() {
+  // Update the countdown display on the page
+  const countdownDisplay = document.getElementById("countdownDisplay");
+  countdownDisplay.textContent = `Loading in ... ${countdown} `;
+}
