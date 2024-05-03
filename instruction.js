984375b5862adaa7f959c2e86f1753d541f8b101
MphoVersace <mtlomelompho@gmail.com>
2024-05-03 15:08:05 +0200

    linked pages

--- instruction.js
+++ instruction.js
@@ -1,13 +1,13 @@
 function quitPage() {
-    window.location.href = "";
-    // window.close();
-  }
-  
-  function goBack() {
-    // window.location.href = "https://youtu.be/QtXby3twMmI?si=a7y9XcFHzHfwpcqA";
-    window.location.href = "player.html";
-  }
-  
-  function beginGame() {
-    window.location.href = "game.html";
-  }
+  window.location.href = "";
+  // window.close();
+}
+
+function goBack() {
+  // window.location.href = "https://youtu.be/QtXby3twMmI?si=a7y9XcFHzHfwpcqA";
+  window.location.href = "index.html";
+}
+
+function beginGame() {
+  window.location.href = "game.html";
+}
